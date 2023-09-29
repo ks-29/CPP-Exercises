4bda20c8e1f185d2ebf313200ddc7aba1bd43bb1
ks-29 <144964854+ks-29@users.noreply.github.com>
2023-09-29 08:18:38 +0800

    Add files via upload

---
+++ exercise6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	double Salary,Day;
+	
+	cout<<"Enter Your Salary:";
+	cin>>Salary;
+	
+		cout<<"Enter Your Day:";
+	cin>>Day;
+	
+
+	
+	cout<<"Total Salary:" <<Salary*Day ;
+	
+	return 0;
+}
